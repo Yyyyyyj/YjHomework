47d2a9e8d798e606e2366e6cd0c2eb105097d97e
youjie <youjie_iris@163.com>
2020-03-21 21:36:13 +0800

    好像没传对？

---
+++ homework/src/main/java/com/yj/homework/model/Task.java
@@ -0,0 +1,35 @@
+package com.yj.homework.model;
+
+public class Task {
+
+	private Long id;
+
+    private String content;
+    
+    private String createdTime;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getCreatedTime() {
+		return createdTime;
+	}
+
+	public void setCreatedTime(String createdTime) {
+		this.createdTime = createdTime;
+	}
+    
+}
